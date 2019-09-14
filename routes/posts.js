af9b60c715d2eae7fa26f9f26838956f37caff6a
Bartłomiej Mazur <ab.mazur@gmail.com>
2019-09-14 09:42:38 +0200

    Added PATCH and DELETE methods

--- routes/posts.js
+++ routes/posts.js
@@ -25,5 +25,29 @@ router.post('/', async (req, res, next) => {
 });
 
 
+//
+router.patch('/:titleSearch', async (req, res, next) => {
+  const titleSearch = req.params.titleSearch;
+  const {title, body} = req.body;
+  const regexSearch = RegExp(titleSearch, 'i')
+  try {
+    const postsUpdate = await Posts.updateOne({title: regexSearch}, {title, body})
+    res.json(postsUpdate);
+  } catch (error) {
+    res.json(error);
+  }
+});
+
+router.delete('/:titleSearch', async (req, res, next) => {
+  const titleSearch = req.params.titleSearch;
+  const regexSearch = RegExp(titleSearch, 'i')
+  try {
+    const postsDelete = await Posts.deleteOne({title: regexSearch});
+    console.log("działa");
+    res.json(postsUpdate);
+  } catch (error) {
+    res.json(error);
+  }
+});
 
 module.exports = router;
